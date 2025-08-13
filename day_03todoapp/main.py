6b5f07931736ca92d4d22fdfee0d26f5b3a11d2b
engrtee <tosin.ilesanmi193@gmail.com>
2025-08-13 21:07:08 +0100

    Developed a functional todo app

--- day_03todoapp/main.py
+++ day_03todoapp/main.py
@@ -2,8 +2,36 @@
 #Step 1 create a l
 b = []
 
-for i in range(5):
-	a = input("Enter your to do: ")
 
+
+def addtask():
+	a = input("Enter your to do: ")
 	b.append(a)
+	
+def printtask():
 	print(b)
+	
+def deletetask():
+	b.pop()
+
+
+# Task manager
+print('Add Task: Press 1 for this feature')
+print('Show Tasks: Press 2 for this feature')
+print('Delete task: Press 3 for this feature')
+
+while True:
+	choice =input('Enter your Input number between 1-3: ')
+	if choice == "1":
+			addtask()
+	elif choice == "2":
+			printtask()
+	elif choice == "3":
+			deletetask()
+	
+	nexttask = input('Do you want to enter another task yes/no: ')
+	if nexttask == 'no':
+					break
+				
+	
+	
