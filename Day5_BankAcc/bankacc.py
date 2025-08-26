fa3539077035ca5cf2337e02ad0a88940e842751
engrtee <tosin.ilesanmi193@gmail.com>
2025-08-26 22:32:49 +0100

    Created a Bank account simulator using Python class

---
+++ Day5_BankAcc/bankacc.py
@@ -0,0 +1,10 @@
+class Bankacc():
+	def __init__(self, acctno,name):
+		self.acctno = acctno
+		self.name = name
+		
+p1 =Bankacc("2020559115","Tosin Ilesanmi")
+p2 =Bankacc("2020559118","Kehinde Ilesanmi")
+p3=Bankacc("2020559111","Tolu Ilesanmi")
+
+print(p2.acctno)
